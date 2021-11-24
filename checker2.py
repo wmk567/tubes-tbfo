16a70ea9df97732fec0fde3bda7631567bbbcb78
wmk567 <16520402@std.stei.itb.ac.id>
2021-11-24 10:43:23 +0700

    mengecek per baris

--- checker2.py
+++ checker2.py
@@ -6,7 +6,9 @@ import simpanparse2
 simpan = sys.argv[1]
 R = CNFtoDict.CNFtoDict("cnf.txt")
 w = lexer_2.lexer(simpan)
+
 finale = simpanparse2.parseAkhir(w,R)
+
 if finale:
     print("Diterima")
 else:
