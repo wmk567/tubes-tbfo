282b42560bc697adf625b72d26a2e84571bf6cce
wmk567 <16520402@std.stei.itb.ac.id>
2021-11-24 10:43:05 +0700

    mengecek per file

--- checker1.py
+++ checker1.py
@@ -6,7 +6,9 @@ import CNFtoDict
 simpan = sys.argv[1]
 R = CNFtoDict.CNFtoDict("cnf.txt")
 w = lexer_1.lexer(simpan)
+
 finale = simpanparse.cykParse(w,R)
+
 if finale:
     print("Diterima")
 else:
